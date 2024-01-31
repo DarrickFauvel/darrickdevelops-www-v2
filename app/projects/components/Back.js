27d22df4e71ef2e6bcfeaa245ffd3ef48903104f
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-01-31 04:07:35 -0500

    Fix back link scrolling

--- app/projects/components/Back.js
+++ app/projects/components/Back.js
@@ -4,5 +4,7 @@ import { useRouter } from "next/navigation"
 export default function Back() {
   const router = useRouter()
 
-  return <button onClick={() => router.back()}>&lt; Go Back</button>
+  return (
+    <button onClick={() => router.replace("/#portfolio")}>&lt; Go Back</button>
+  )
 }
