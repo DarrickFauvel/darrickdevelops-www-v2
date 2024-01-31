b48e679b0becd21593f8150f10ac976fce3996c0
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-01-31 03:45:01 -0500

    Fix navbar responsiveness

--- components/Navbar/Nav/index.jsx
+++ components/Navbar/Nav/index.jsx
@@ -3,7 +3,7 @@ import sectionTitles from "@/components/shared/sectionTitles"
 
 const Nav = () => {
   return (
-    <nav className="hidden flex-wrap items-center justify-center text-base gap-5 min-[520px]:flex min-[520px]:mr-auto min-[520px]:ml-4 min-[520px]:py-1 min-[520px]:pl-4 min-[520px]:border-l-[1px] min-[520px]:border-solid min-[520px]:border-zinc-600">
+    <nav className="hidden flex-wrap items-center justify-center text-base gap-5 min-[575px]:flex min-[575px]:mr-auto min-[520px]:ml-4 min-[520px]:py-1 min-[520px]:pl-4 min-[520px]:border-l-[1px] min-[520px]:border-solid min-[520px]:border-zinc-600">
       {sectionTitles.map((sectionTitle) => (
         <NavLink sectionTitle={sectionTitle} key={sectionTitle} />
       ))}
