cae851cc0f7d426ee74387680391772e3d51671a
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-02-01 20:26:34 -0500

    Add scroll offset

--- components/FooterSection/index.js
+++ components/FooterSection/index.js
@@ -1,22 +1,25 @@
 import Container from "../shared/Container"
+import ScrollOffset from "../shared/ScrollOffset"
 
 const FooterSection = () => {
   const classesContainer =
     "flex flex-col items-center flex-wrap w-full md:flex-row md:justify-center md:max-w-3xl lg:max-w-5xl"
 
   return (
-    <footer className="footer">
-      <Container>
-        <div className={`${classesContainer}`}>
-          <div className="text-center font-light text-white mb-4 md:mb-0">
-            Made with{" "}
-            <span role="img" aria-label="heart emoji">
-              ❤️
-            </span>{" "}
-            in Massachusetts.
+    <footer>
+      <ScrollOffset>
+        <Container>
+          <div className={`${classesContainer}`}>
+            <div className="text-center font-light text-white mb-4 md:mb-0">
+              Made with{" "}
+              <span role="img" aria-label="heart emoji">
+                ❤️
+              </span>{" "}
+              in Massachusetts.
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </ScrollOffset>
     </footer>
   )
 }
