8a9fd8fa577a5a4e372c6dc2c738366fa5cd20de
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-02-08 04:09:10 -0500

    Create button link component

--- app/projects/components/ButtonLink.js
+++ app/projects/components/ButtonLink.js
@@ -1,18 +1,12 @@
 import Image from "next/image"
 
-function ButtonLink() {
+function ButtonLink({ link, children }) {
   return (
     <a
       className="flex items-center gap-2 bg-gray-800 py-2 px-6 rounded border-[1px] border-gray-700 transition duration-500 hover:bg-gray-700 hover:text-white"
-      href={project.liveUrl}
+      href={link}
       target="_blank">
-      <Image
-        src="/netlify-logo-cropped.png"
-        alt="Netlify logo"
-        width={32}
-        height={32}
-      />
-      <span>Demo</span>
+      {children}
     </a>
   )
 }
