0f086af707c13361d6633283f058914c8238c1be
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-02 17:27:06 -0400

    Update meta title and description

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,9 @@ import FooterSection from "@/components/FooterSection"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Darrick Develops | Software Developer",
-  description: "Software development is my jam.",
+  title: "Darrick Develops | Software Engineer",
+  description:
+    "Darrick Develops is your go-to source for professional software engineering solutions. Explore a portfolio showcasing innovative projects, programming expertise, and a passion for technology. Let’s bring your ideas to life!",
 }
 
 export default function RootLayout({ children }) {
